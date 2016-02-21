bccff7453a2befa3b638a10634e4ebb9360f3a29
Jakub Lewek <jakublewek@icloud.com>
2016-02-21 21:12:46 +0100

    add Xcode project

---
+++ GNotes/GNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GNotes
+//
+//  Created by Jakub Lewek on 21.02.2016.
+//  Copyright © 2016 educativo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
